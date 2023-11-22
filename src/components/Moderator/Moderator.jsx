e08985fb56ef66c5ec4a3fabf47462b15ba482c9
Muslimov-Xasan <muslimovxasan050@gmail.com>
2023-11-22 15:54:05 +0500

    go

--- src/components/Moderator/Moderator.jsx
+++ src/components/Moderator/Moderator.jsx
@@ -7,6 +7,15 @@ import "./Moderator.css";
 const Moderator = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
+  // State for form inputs within the modal
+  const [productName, setProductName] = useState("");
+  const [comment, setComment] = useState("");
+  const [price, setPrice] = useState("");
+  const [region, setRegion] = useState("");
+  const [weight, setWeight] = useState("");
+  const [district, setDistrict] = useState("");
+  const [village, setVillage] = useState("");
+
   const openModal = () => {
     setModalIsOpen(true);
   };
@@ -15,11 +24,16 @@ const Moderator = () => {
     setModalIsOpen(false);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform any additional actions here if needed
+    closeModal();
+  };
   Modal.setAppElement("#root"); // Assuming your root element has the id "root"
 
   return (
     <>
-      <div className="contianer">
+      <div className="contianer-fulid">
         <img src={Logo} alt="logo" width={164} height={42} />
         <h2 className="moderator-title">Yangi qo’shilgan</h2>
         <div className="all">
@@ -27,13 +41,10 @@ const Moderator = () => {
             <li className="product-item" onClick={openModal}>
               <img src={Onion} alt="Onion" width={170} height={160} />
               <div className="wrapper-location">
-                <h2 className="product-title">Piyoz</h2>
+                <h2 className="product-title">Piyoz {productName}</h2>
                 <p className="product-text">
-                  Poliz ekinlari - oziq-ovqat, yem-xashak va texnika
-                  maqsadlarida ekiladigan, palak otib oʻsadigan madaniy ekinlar
-                  guruhi. Ayrim olimlar Poliz ekinlariga faqat tarvuz, kovun va
-                  qovoqnn kiritadilar; chirmashib yoki yerda yotib usadi.
-                </p>
+                Poliz ekinlari - oziq-ovqat, yem-xashak va texnika maqsadlarida ekiladigan, palak otib oʻsadigan madaniy ekinlar guruhi. Ayrim olimlar Poliz ekinlariga faqat tarvuz, kovun va qovoqnn kiritadilar; chirmashib yoki yerda yotib usadi.
+                  {comment}</p>
                 <div className="voydod">
                   <img
                     className="location-icon"
@@ -43,9 +54,11 @@ const Moderator = () => {
                     height={23}
                   />
                   <div className="go">
-                    <p className="location-word">Qashqadaryo v. Yakkabog’ t.</p>
-                    <p className="kg">3.5 tonna</p>
-                    <p className="price">2 200 so’m</p>
+                    <p className="location-word">Qashqadaryo v. Yakkabog’ t.
+                     {region} {district} {village}
+                     </p>
+                    <p className="kg">33 kg {weight}</p>
+                    <p className="price">4 000 {price}</p>
                   </div>
                 </div>
               </div>
@@ -69,6 +82,8 @@ const Moderator = () => {
                 className="input-product"
                 type="text"
                 placeholder="Kartoshka"
+                value={productName}
+                onChange={(e) => setProductName(e.target.value)}
               />
             </div>
             <div>
@@ -106,50 +121,76 @@ const Moderator = () => {
 
           <form className="form-price">
             <div>
-              <label htmlFor="Narx (so’m) *">Narx (so’m) *</label>
-              <input className="input-price" type="text" placeholder="3 500" />
+              <label htmlFor="Narxi (so'm) *">Narxi (so'm) *</label>
+              <input
+                className="input-price"
+                type="text"
+                placeholder="3 500"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
             </div>
             <div>
               <label htmlFor="Miqdori (tonna) *">Miqdori (tonna) *</label>
-              <input className="input-price" type="text" placeholder="3" />
+              <input
+                className="input-price"
+                type="text"
+                placeholder="3"
+                value={weight}
+                onChange={(e) => setWeight(e.target.value)}
+              />
             </div>
           </form>
 
-          <p className="comment">Izoh *</p>
-          <p className="comment-word">
-            Kartoshka dunyoning turli mintaqalarida etishtiriladi, ularning eng
-            yirik ishlab chiqaruvchilari Xitoy, Hindiston, Rossiya va AQShdir.
-            Ular odatda bahorda ekilgan va kuzda yig'ib olinadi. Kartoshka
-            moslashuvchanligi bilan mashhur va uni turli iqlim va tuproq
-            turlarida etishtirish mumkin. Kartoshka turli zararkunandalar va
-            kasalliklarga, jumladan kartoshka qo'ng'izlari, kech blight va erta
-            blightga moyil. Fermerlar ko'pincha o'z ekinlarini himoya qilish
-            uchun zararkunandalarga qarshi kurash usullari va kasalliklarga
-            chidamli kartoshka navlarini qo'llashadi.
-          </p>
+          <p className="comment">izoh *</p>
+          <textarea
+            className="comment-word"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
         </div>
         <div>
           <form className="province-form">
-            <label htmlFor="Viloyat *">Viloyat *</label>
-            <select className="province-select">
-              <option value="Qashqadaryo">Qashqadaryo</option>
-              <option value="Surxondaryo">Surxondaryo</option>
-              <option value="Namangan">Namangan</option>
-              <option value="Samarqand">Samarqand</option>
-              <option value="Andijon">Andijon</option>
-            </select>
-            <label htmlFor="Tuman *">Tuman *</label>
-            <select className="province-select">
-              <option value="Qashqadaryo">Qashqadaryo</option>
-              <option value="Surxondaryo">Surxondaryo</option>
-              <option value="Namangan">Namangan</option>
-              <option value="Samarqand">Samarqand</option>
-              <option value="Andijon">Andijon</option>
-            </select>
-            <label htmlFor="Qishloq / Mahalla nomi">
-              Qishloq / Mahalla nomi
-            </label>
-            <input type="text" placeholder="G’urjob" />
+            <div>
+              <label htmlFor="Viloyat *">Viloyat *</label>
+              <select
+                className="province-select"
+                value={region}
+                onChange={(e) => setRegion(e.target.value)}
+              >
+                <option value="Qashqadaryo">Qashqadaryo</option>
+                <option value="Surxondaryo">Surxondaryo</option>
+                <option value="Namangan">Namangan</option>
+                <option value="Samarqand">Samarqand</option>
+                <option value="Andijon">Andijon</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="Tuman *">Tuman *</label>
+              <select className="province-select" 
+                  value={district}
+                  onChange={(e) => setDistrict(e.target.value)}
+              >
+                <option value="Yakkabog’">Yakkabog’</option>
+                <option value="Boysun">Boysun</option>
+                <option value="Jalaquduq">Jalaquduq</option>
+                <option value="Olamazor">Olamazor</option>
+                <option value="Bodomzor">Bodomzor</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="Neighborhood">Qishloq/tuman nomi</label>
+              <input
+                className="neighborhood-input"
+                type="text"
+                id="Neighborhood"
+                placeholder="G’urjob"
+                autoComplete="off"
+                value={village}
+                onChange={(e) => setVillage(e.target.value)}
+                //(e) => setWeight(e.target.value)
+              />
+            </div>
           </form>
 
           <p className="contact-text">Aloqa uchun qo’shimcha telefon raqam</p>
@@ -157,7 +198,12 @@ const Moderator = () => {
 
           <div className="wrapper-button">
             <button className="modal-delete">O’chirish</button>
-            <button className="confirmation-confirmation">Tasdiqlash</button>
+            <button
+              className="confirmation-confirmation"
+              onClick={handleSubmit}
+            >
+              Tasdiqlash
+            </button>
           </div>
         </div>
       </Modal>
