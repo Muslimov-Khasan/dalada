dce2bc030c3ab2ce6261072018d1abe60edfe4d6
Muslimov-Xasan <muslimovxasan050@gmail.com>
2023-11-22 08:57:34 +0500

    updend

--- src/components/Moderator/Moderator.jsx
+++ src/components/Moderator/Moderator.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import LocationIcon from "../../Assets/img/location.svg";
 import Modal from "react-modal";
-
 import Logo from "../../Assets/img/Logo.svg";
 import Onion from "../../Assets/img/Onion.png";
 import "./Moderator.css";
@@ -15,6 +14,7 @@ const Moderator = () => {
   const closeModal = () => {
     setModalIsOpen(false);
   };
+
   Modal.setAppElement("#root"); // Assuming your root element has the id "root"
 
   return (
@@ -61,32 +61,104 @@ const Moderator = () => {
         <button className="close-btn" onClick={closeModal}>
           &#10006;
         </button>
-
-        <form className="form-product">
-          <div>
-            <label htmlFor="Nomi *">Nomi *</label>
-            <input
-              className="input-product"
-              type="text"
-              placeholder="Kartoshka"
-            />
-          </div>
-          <div>
-            <label htmlFor="Kategoriya *">Kategoriya *</label>
-            <select className="products-select">
-              <option value="Dala maxsilotlari">Dala maxsilotlari</option>
-              <option value="Dala maxsilotlari">Ekin maxsilotlari</option>
-              <option value="Dala maxsilotlari">Meva maxsilotlari</option>
-            </select>
-          </div>
-        </form>
-        <div className="imgages">
-          <form className="form-lord">
-            <h2 className="label-img" >Rasm *</h2>
-            <img src="https://placehold.co/100" alt="" width={96} height={96} />
-            <img src="https://placehold.co/100" alt="" width={96} height={96} />
-            <img src="https://placehold.co/100" alt="" width={96} height={96} />
+        <div className="good">
+          <form className="form-product">
+            <div>
+              <label htmlFor="Nomi *">Nomi *</label>
+              <input
+                className="input-product"
+                type="text"
+                placeholder="Kartoshka"
+              />
+            </div>
+            <div>
+              <label htmlFor="Kategoriya *">Kategoriya *</label>
+              <select className="products-select">
+                <option value="Dala maxsilotlari">Dala maxsilotlari</option>
+                <option value="Dala maxsilotlari">Ekin maxsilotlari</option>
+                <option value="Dala maxsilotlari">Meva maxsilotlari</option>
+              </select>
+            </div>
           </form>
+          <div className="imgages">
+            <form className="form-lord">
+              <h2 className="label-img">Rasm *</h2>
+              <img
+                src="https://placehold.co/100"
+                alt=""
+                width={96}
+                height={96}
+              />
+              <img
+                src="https://placehold.co/100"
+                alt=""
+                width={96}
+                height={96}
+              />
+              <img
+                src="https://placehold.co/100"
+                alt=""
+                width={96}
+                height={96}
+              />
+            </form>
+          </div>
+
+          <form className="form-price">
+            <div>
+              <label htmlFor="Narx (so’m) *">Narx (so’m) *</label>
+              <input className="input-price" type="text" placeholder="3 500" />
+            </div>
+            <div>
+              <label htmlFor="Miqdori (tonna) *">Miqdori (tonna) *</label>
+              <input className="input-price" type="text" placeholder="3" />
+            </div>
+          </form>
+
+          <p className="comment">Izoh *</p>
+          <p className="comment-word">
+            Kartoshka dunyoning turli mintaqalarida etishtiriladi, ularning eng
+            yirik ishlab chiqaruvchilari Xitoy, Hindiston, Rossiya va AQShdir.
+            Ular odatda bahorda ekilgan va kuzda yig'ib olinadi. Kartoshka
+            moslashuvchanligi bilan mashhur va uni turli iqlim va tuproq
+            turlarida etishtirish mumkin. Kartoshka turli zararkunandalar va
+            kasalliklarga, jumladan kartoshka qo'ng'izlari, kech blight va erta
+            blightga moyil. Fermerlar ko'pincha o'z ekinlarini himoya qilish
+            uchun zararkunandalarga qarshi kurash usullari va kasalliklarga
+            chidamli kartoshka navlarini qo'llashadi.
+          </p>
+        </div>
+        <div>
+          <form className="province-form">
+            <label htmlFor="Viloyat *">Viloyat *</label>
+            <select className="province-select">
+              <option value="Qashqadaryo">Qashqadaryo</option>
+              <option value="Surxondaryo">Surxondaryo</option>
+              <option value="Namangan">Namangan</option>
+              <option value="Samarqand">Samarqand</option>
+              <option value="Andijon">Andijon</option>
+            </select>
+            <label htmlFor="Tuman *">Tuman *</label>
+            <select className="province-select">
+              <option value="Qashqadaryo">Qashqadaryo</option>
+              <option value="Surxondaryo">Surxondaryo</option>
+              <option value="Namangan">Namangan</option>
+              <option value="Samarqand">Samarqand</option>
+              <option value="Andijon">Andijon</option>
+            </select>
+            <label htmlFor="Qishloq / Mahalla nomi">
+              Qishloq / Mahalla nomi
+            </label>
+            <input type="text" placeholder="G’urjob" />
+          </form>
+
+          <p className="contact-text">Aloqa uchun qo’shimcha telefon raqam</p>
+          <p className="contact-text">+998 94 332 00 16</p>
+
+          <div className="wrapper-button">
+            <button className="modal-delete">O’chirish</button>
+            <button className="confirmation-confirmation">Tasdiqlash</button>
+          </div>
         </div>
       </Modal>
     </>
